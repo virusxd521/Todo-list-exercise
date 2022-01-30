7a739a9d3e24d45e679538528fe603a56c039333
Daniel <virusxd521@gmail.com>
2022-01-30 16:11:44 +0200

    adding JavaScript to the project

--- dist/javascript/script.js
+++ dist/javascript/script.js
@@ -1,4 +1,22 @@
-// The variables
-const inputSubmit = document.querySelector
+// swal library
+import swal from '../../node_modules/sweetalert/typings/sweetalert';
 
-console.log("sss");
+
+
+
+// element variable
+const inputSubmit = document.querySelector(".submit");
+const item = document.querySelector("#item");
+const quantity = document.querySelector("#quant");
+const elementsList = document.querySelector(".elements-list");
+
+// submiting the form and adding the values to the shopping list
+inputSubmit.addEventListener("click", event => {
+    event.preventDefault();
+    if(item.value === ""){
+        swal.swal("Hello world!");
+        console.log(item.value);
+    console.log(quantity.value);
+    }
+
+})
